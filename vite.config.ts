fee0c45a5d1e4e98dcf626c417ddf89ab8778b73
marvinlintw <marvinlintw@gmail.com>
2026-02-16 16:12:56 +0800

    fix ts errors and config base path

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,9 @@
+// vite.config.ts
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SeatingLayout/', // <--- 請新增這一行，必須跟您的 GitHub 專案名稱一模一樣
+  // ⚠️ 關鍵：這裡必須是您的 GitHub Repository 名稱，前後都要有斜線
+  base: '/SeatingLayout/', 
 })
